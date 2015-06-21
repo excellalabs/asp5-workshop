aa407c2b8af4cebb64b8b3b6e9df6c9153ee1417
bdpalladino <Brian.Palladino@excella.com>
2015-06-21 12:58:30 -0400

    Initial commit

---
+++ 1. Getting Started/OneManBlog/src/OneManBlog/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace OneManBlog
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        /* Boileplate code generated with new app
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+        --- */
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello world!");
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync(" This is sort of an echo Hello world!");
+            });
+        }
+    }
+}
---
+++ 1. Getting Started/README.md
@@ -0,0 +1,8 @@
+## 1. Getting Started README
+
+This directory contains code for a new "Hello World" ASP.NET 5 app.
+
+Content based on Tomas Jansson's blog post: 
+http://blog.tomasjansson.com/asp-net-5-the-pipeline/
+
+For more information on how to configure your application, visit: http://go.microsoft.com/fwlink/?LinkID=398940
