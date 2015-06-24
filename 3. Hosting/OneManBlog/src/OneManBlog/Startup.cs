c038585dfeea909c1a36640eaa10c357ec2501de
Lougenia Bailey <lougenia.bailey@excella.com>
2015-06-24 16:30:16 -0400

    Copied Web Api solution to self-hosting folder.

---
+++ 3. Hosting/OneManBlog/src/OneManBlog/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace OneManBlog
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        /* Boileplate code generated with new app
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+        --- */
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello world!");
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync(" This is sort of an echo Hello world!");
+            });
+        }
+    }
+}
