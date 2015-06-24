1a23fb951388821e1fb6bfa81eea2bc6c4c2d1a5
Lougenia Bailey <lougenia.bailey@excella.com>
2015-06-24 11:23:59 -0400

    Added Web API references, Controllers and Models folder, TodoController and TodoItem files, updated README file.

---
+++ 2. WebApi/OneManBlog/src/OneManBlog/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OneManBlog.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Title}";
+        }
+    }
+}
